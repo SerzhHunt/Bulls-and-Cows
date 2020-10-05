e5131114b99c0e0a371f90be05c37804f24c87a3
Sergey Rakov <rakovsergey94@gmail.com>
2020-10-05 23:25:17 +0300

    stage5 -> Successfully a step for modifying the generation of a pseudo-random number. It has already been done, there are no changes.

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,13 +1,9 @@
 package bullscows;
 
-
 import bullscows.game.Game;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Game game = new Game();
         game.gamePlay();
     }
